d4faf19e839c013212440cb2d5da8042e91ee216
JazepsA <jazepsaugustans26@gmail.com>
2024-09-19 09:58:43 +0300

    Update polimorfisms.py

--- polimorfisms.py
+++ polimorfisms.py
@@ -2,10 +2,17 @@ class Pupil:
     __age=0 #private
     #setter piekļuve datiem
     def set_age(self,value:int):
-        self.__age=value
+        if value >=0:
+            self.__age=value
+        else:
+            print("Vecums nevar būt negatīvs.")
         #getter vērtības piešķiršana
-
+    def get_age(self):
+        return self.__age
 pupil=Pupil()
-pupil.__age=-1
-print(pupil.__age)
-pupil.set_age()
+pupil.set_age(-1)
+pupil.set_age(10)
+
+#pupil.__age=-1
+print(pupil.get_age())
+#pupil.set_age()
